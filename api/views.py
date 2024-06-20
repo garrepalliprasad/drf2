cccf5967670e3eb9e8cbc4495a45e59a5cf0b8a5
garrepalliprasad <garrepalli.prasad@gmail.com>
2024-06-20 18:45:08 +0530

    generic api view get

--- api/views.py
+++ api/views.py
@@ -1,61 +1,11 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from .models import Student
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin,RetrieveModelMixin,CreateModelMixin,UpdateModelMixin,DestroyModelMixin
 from .serializers import StudentSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-class StudentApi(APIView):    
+from .models import Student
+class StudentApi(GenericAPIView,ListModelMixin,RetrieveModelMixin,CreateModelMixin,UpdateModelMixin,DestroyModelMixin):
+    serializer_class=StudentSerializer
+    queryset=Student.objects.all()
+    lookup_field='id'
 
-    def get(self,request,id=None,format=None):
-        if id is not None:
-            try:
-                student=Student.objects.get(id=id)
-                student_serialized=StudentSerializer(student)
-                return Response(student_serialized.data,status=status.HTTP_200_OK)
-            except Student.DoesNotExist:
-                return Response({'Failed':'Student Does not Exists'},status=status.HTTP_400_BAD_REQUEST)
-        else:
-            students=Student.objects.all()
-            student_serialized=StudentSerializer(students,many=True)
-            return Response(student_serialized.data,status=status.HTTP_200_OK)
-        
-    def post(self,request,format=None):
-        student_serialized=StudentSerializer(data=request.data)
-        if student_serialized.is_valid():
-            student_serialized.save()
-            return Response({'Success':'Student Created'},status=status.HTTP_201_CREATED)
-        return Response(student_serialized.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def put(self,request,id=None,format=None):
-        try:
-            student=Student.objects.get(id=id)
-            student_serialized=StudentSerializer(student,data=request.data)
-            if student_serialized.is_valid():
-                student_serialized.save()
-                return Response({'Success':'Student Updated Fully'},status=status.HTTP_200_OK)
-        except Student.DoesNotExist:
-            return Response({'Failed':'Student Not Found'},status=status.HTTP_400_BAD_REQUEST)
-        return Response(student_serialized.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def patch(self,request,id=None,format=None):
-        try:
-            student=Student.objects.get(id=id)
-            student_serialized=StudentSerializer(student,data=request.data,partial=True)
-            if student_serialized.is_valid():
-                student_serialized.save()
-                return Response({'Success':'Student Updated Partially'},status=status.HTTP_200_OK)
-        except Student.DoesNotExist:
-            return Response({'Failed':'Student Not Found'},status=status.HTTP_400_BAD_REQUEST)
-        return Response(student_serialized.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self,request,id=None,format=None):
-        try:
-            student=Student.objects.get(id=id)
-            student.delete()
-            return Response({'Success':'Student Deleted'},status=status.HTTP_200_OK)
-        except Student.DoesNotExist:
-            return Response({'Failed':'Student Not Found'},status=status.HTTP_400_BAD_REQUEST)
-        
-    
-
+    def get(self,request,id=None,*args,**kwargs):
+        return self.retrieve(request,id,*args,**kwargs)
